56d93efc29ec92a7d80cad8a7f61a5a459034435
TheENDima <theendima@gmail.com>
2026-02-09 23:45:24 +0200

    add second test on pythest

--- text_fixtures.py
+++ text_fixtures.py
@@ -1,21 +1,18 @@
 import pytest
 
 @pytest.fixture
-def browser():
-    print("Браузер")
-
-
-@pytest.fixture
-def login_page(browser):
+def login_page(browser, browser_context_args):
     print("страница логин")
-    pass
+
 
 @pytest.fixture
 def user():
-    return "username","password"
+    print("Юзверь")
+    return "username", "password"
 
 
 def test_login(login_page, user):
     username, password = user
     assert username == "username"
     assert password == "password"
+
